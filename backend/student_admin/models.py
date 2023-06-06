f78e420e7b6ac3b3e914f9f59b3b5a4dd56b0f7b
Izaan Aamir <65916412+izaanaamir@users.noreply.github.com>
2023-06-06 19:44:31 +0200

    connected to backend

--- backend/student_admin/models.py
+++ backend/student_admin/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.db import models
 
+
 class Student(models.Model):
     StudentID = models.AutoField(primary_key=True)
     FirstName = models.CharField(max_length=255, null=True)
@@ -13,11 +14,11 @@ class Student(models.Model):
     StudentStatus = models.CharField(
         max_length=255,
         choices=(
-            ('Value1', 'Value1'),
-            ('Value2', 'Value2'),
-            ('Value3', 'Value3'),
+            ("Value1", "Value1"),
+            ("Value2", "Value2"),
+            ("Value3", "Value3"),
         ),
-        null=True
+        null=True,
     )
     SpecialArrangement = models.CharField(max_length=255, null=True)
     PersonalEmail = models.CharField(max_length=255, null=True)
@@ -28,5 +29,4 @@ class Student(models.Model):
         return f"{self.FirstName} {self.LastName}"
 
     class Meta:
-        db_table = 'students'
-
+        db_table = "students"

9ac9aa3fff505a04fa2ce99eca06b8820faeaaf2
Izaan Aamir <65916412+izaanaamir@users.noreply.github.com>
2023-06-06 19:39:16 +0200

    Update views.py

--- backend/student_admin/views.py
+++ backend/student_admin/views.py
@@ -5,7 +5,3 @@ from django.http import HttpResponse
 def student_list(request):
     cell_number = Student.objects.filter(FirstName='Izaan').values_list('CellphoneNumber', flat=True).first()    
     return HttpResponse(cell_number)
-
-
-
-

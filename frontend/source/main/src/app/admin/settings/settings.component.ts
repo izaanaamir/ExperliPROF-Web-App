8b479eb6c40145b8701145b071233cedb95ac889
Izaan Aamir <65916412+izaanaamir@users.noreply.github.com>
2023-06-20 12:45:21 +0200

    settings changes

--- frontend/source/main/src/app/admin/settings/settings.component.ts
+++ frontend/source/main/src/app/admin/settings/settings.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit {
   breadscrums = [
     {
       title: '',
@@ -14,7 +15,52 @@ export class SettingsComponent {
     },
   ];
 
-  constructor() {
-    // constructor
+  email!: string;
+  currentPassword!: string;
+  newPassword!: string;
+  firstName!: string;
+  lastName!: string;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadUserData();
+  }
+
+  loadUserData() {
+    var url = "http://localhost:8000/api/user/get_user_info/" + localStorage.getItem("user_uuid")
+
+    this.http.get<any>(url).subscribe(
+      (response) => {
+        var data = response['data'];
+        console.log(data);
+        this.email = data['email'];
+        this.firstName = data['firstName'];
+        this.lastName = data['lastName'];
+      },
+      (error) => {
+        // Handle error here
+      }
+    );
+  }
+
+  saveSecuritySettings() {
+    // Perform API request to save security settings
+    const payload = {
+      email: this.email,
+      currentPassword: this.currentPassword,
+      newPassword: this.newPassword,
+    };
+    var url = "http://localhost:8000/api/user/get_user_info/" + localStorage.getItem("user_uuid")
+    this.http.post(url, payload).subscribe(
+      (response) => {
+        // Handle success response
+      this.currentPassword = '';
+      this.newPassword = '';
+      },
+      (error) => {
+        // Handle error response
+      }
+    );
   }
 }
